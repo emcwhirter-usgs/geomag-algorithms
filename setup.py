bf1f2b4cf8fec5c747ca9dd1b3c8e3ea5490a282
Eddie McWhirter <emcwhirter@usgs.gov>
2015-07-27 10:22:36 -0600

    Increment version to 0.0.1.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='geomag-algorithms',
-    version='0.0.0',
+    version='0.0.1',
     desription='USGS Geomag IO Library',
     url='https://github.com/usgs/geomag-algorithms',
     packages=[
